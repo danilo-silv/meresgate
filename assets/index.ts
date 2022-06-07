8ebb28957ea7804cea69b39090341951524fcf30
Sued Muniz <sued.muniz.1@gmail.com>
2022-06-07 16:58:04 -0300

    Adding new images and exporting them.

--- assets/index.ts
+++ assets/index.ts
@@ -9,31 +9,35 @@ import DogPhoto from './dog-photo.png'
 import Dog from './dog.png'
 import Fone from './fone.png'
 import GrayPaw from './gray-paw.png'
+import GreyPawUserProfile from './grey-paw-user-profile.png'
 import Guilherme from './guilherme.png'
 import Hand from './hand.png'
 import Laura from './laura.png'
+import MailIconWhite from './mail-icon-white.png'
+import ManAndCat from './man-and-cat.png'
 import NoBgBluePaw from './no-bg-blue-paw.png'
 import Sued from './sued.png'
 import Vaccine from './vaccine.png'
 import Victoria from './victoria.png'
-import WhitePaw from './white-paw.png'
 import WomanAndPet from './woman-and-pet.png'
 import WomanExample from './woman-example.png'
 import YellowPaw from './yellow-paw.png'
 
 export {
+  MailIconWhite,
   BluePaw,
   YellowPaw,
   GrayPaw,
+  BlueLeftPaw,
+  GreyPawUserProfile,
   DogPaw,
   Dog,
   Hand,
   Vaccine,
   WomanExample,
   Fone,
-  WhitePaw,
   WomanAndPet,
-  BlueLeftPaw,
+  ManAndCat,
   BlueCamera,
   BlueHand,
   BluePawCamera,
