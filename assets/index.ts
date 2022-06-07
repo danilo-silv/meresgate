3c73e783230119b300fa9c28401d007365125f0c
Sued Muniz <sued.muniz.1@gmail.com>
2022-06-07 04:02:57 -0300

    Adding new images and exporting them from assets.

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import BluePaw from './blue-paw.png'
+import YellowPaw from './yellow-paw.png'
+import GrayPaw from './gray-paw.png'
+import DogPaw from './dog-paw.png'
+import Dog from './dog.png'
+import Hand from './hand.png'
+import Vaccine from './vaccine.png'
+import WomanExample from './woman-example.jpg'
+import Fone from './fone.png'
+
+export {
+    BluePaw,
+    YellowPaw,
+    GrayPaw,
+    DogPaw,
+    Dog,
+    Hand,
+    Vaccine,
+    WomanExample,
+    Fone
+}
