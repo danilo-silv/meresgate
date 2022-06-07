847314916a43ac067065ecbecea53b71f6038c42
Sued Muniz <sued.muniz.1@gmail.com>
2022-06-07 05:36:03 -0300

    Adding and exporting new images.

--- assets/index.ts
+++ assets/index.ts
@@ -7,6 +7,17 @@ import Hand from './hand.png'
 import Vaccine from './vaccine.png'
 import WomanExample from './woman-example.jpg'
 import Fone from './fone.png'
+import WomanAndPet from './woman-and-pet.png'
+import BlueCamera from './blue-camera.png'
+import BlueHand from './blue-hand.png'
+import BluePawCamera from './blue-paw-camera.png'
+import DogPhoto from './dog-photo.png'
+import NoBgBluePaw from './no-bg-blue-paw.png'
+import Sued from './sued.png'
+import Danilo from './danilo.png'
+import Laura from './laura.png'
+import Guilherme from './guilherme.png'
+import Victoria from './victoria.png'
 
 export {
     BluePaw,
@@ -17,5 +28,16 @@ export {
     Hand,
     Vaccine,
     WomanExample,
-    Fone
+    Fone,
+    WomanAndPet,
+    BlueCamera,
+    BlueHand,
+    BluePawCamera,
+    DogPhoto,
+    NoBgBluePaw,
+    Danilo,
+    Sued,
+    Laura,
+    Guilherme,
+    Victoria
 }
