d61cb74be604b7bf3d5e287dbd672f47a1950330
Sued Muniz <sued.muniz.1@gmail.com>
2022-06-20 22:37:23 -0300

    Adding bottom sheet and some features.

--- src/screens/Tabs/PetRegister/PetRegister.tsx
+++ src/screens/Tabs/PetRegister/PetRegister.tsx
@@ -14,22 +14,25 @@ import {
   View,
   VStack
 } from 'native-base'
-import { Alert, Platform, StyleSheet, TouchableOpacity } from 'react-native'
+import { Platform, StyleSheet, TouchableOpacity } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import { theme } from 'src/theme'
-import { AddDogPhoto } from 'assets'
+import { AddDogPhoto, Gallery, RemovePhoto, SheetCamera } from 'assets'
 import { Camera, CameraType } from 'expo-camera'
 import { MaterialIcons } from '@expo/vector-icons'
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet'
 
 export const PetRegister: FunctionComponent = () => {
   const [isVacinated, setIsVacinated] = useState<boolean>(false)
   const [isRescued, setIsRescued] = useState<boolean>(false)
-
-  const camRef = useRef<any>()
   const [isCameraVisible, setIsCameraVisible] = useState<boolean>(false)
   const [hasPermission, setHasPermission] = useState<any>(null)
   const [type, setType] = useState<CameraType>(CameraType.back)
   const [image, setImage] = useState<any>(null)
+
+  const camRef = useRef<any>()
+  const sheetRef = useRef<BottomSheet>(null)
+  const snapPoints = [1, '35%']
 
   const toggleSwitch = (switchKey: string): void =>
     switchKey === 'vacinated' ? setIsVacinated(!isVacinated) : setIsRescued(!isRescued)
@@ -43,8 +46,6 @@ export const PetRegister: FunctionComponent = () => {
       aspect: [4, 3],
       quality: 1
     })
-
-    console.log(result)
 
     if (!result.cancelled) {
       setImage(result.uri)
@@ -64,29 +65,6 @@ export const PetRegister: FunctionComponent = () => {
     setIsCameraVisible(false)
   }
 
-  const handlePetPhoto = () => {
-    Alert.alert(
-      'Selecione',
-      'De onde você irá selecionar a imagem?',
-      [
-        {
-          text: 'Galeria',
-          onPress: () => pickImageFromGallery(),
-          style: 'default'
-        },
-        {
-          text: 'Câmera',
-          onPress: () => setIsCameraVisible(true),
-          style: 'default'
-        }
-      ],
-      {
-        cancelable: true,
-        onDismiss: () => setIsCameraVisible(false)
-      }
-    )
-  }
-
   useEffect(() => {
     ;(async () => {
       const { status } = await Camera.requestCameraPermissionsAsync()
@@ -102,165 +80,210 @@ export const PetRegister: FunctionComponent = () => {
   }
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <Layouts.Internal typeTwo>
-        <VStack alignItems="center" justifyContent="center" paddingX={3} paddingBottom={100}>
-          {isCameraVisible && (
-            <Camera
-              ref={camRef}
-              style={{ width: '100%', height: 540, justifyContent: 'flex-end' }}
-              type={type}>
-              <HStack justifyContent="space-around">
-                <TouchableOpacity
-                  style={{ paddingVertical: 20 }}
-                  onPress={() => {
-                    setType(type === CameraType.back ? CameraType.front : CameraType.back)
-                  }}>
-                  <MaterialIcons name="flip-camera-android" size={36} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{ paddingVertical: 20 }}
-                  onPress={() => {
-                    takePicture()
-                  }}>
-                  <MaterialIcons name="photo-camera" size={36} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{ paddingVertical: 20 }}
-                  onPress={() => {
-                    setIsCameraVisible(false)
-                  }}>
-                  <MaterialIcons name="cancel" size={36} color="white" />
-                </TouchableOpacity>
-              </HStack>
-            </Camera>
-          )}
-          <Text style={styles.createAccountText}>
-            Preencha o máximo de dados a respeito do pet encontrado...
-          </Text>
-          <TouchableOpacity onPress={() => handlePetPhoto()}>
-            <Image
-              source={image ? { uri: image } : AddDogPhoto}
-              alt="dog icon and a camera"
-              mb={3}
-              style={{ width: 125, height: 125, borderRadius: 20 }}
-            />
-          </TouchableOpacity>
-          <Text fontSize={15} color={theme.colors.primary[900]} my={2}>
-            Informações de endereço:
-          </Text>
-
-          <View>
-            <Input
-              placeholder="CEP*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Endereço*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Bairro*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Cidade*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Estado*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Número*"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Text fontSize={15} color={theme.colors.primary[900]} mt={5}>
-              Informações do pet:
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Layouts.Internal typeTwo>
+          <VStack alignItems="center" justifyContent="center" paddingX={3} paddingBottom={100}>
+            {isCameraVisible && (
+              <Camera
+                ref={camRef}
+                style={{ width: '100%', height: 540, justifyContent: 'flex-end' }}
+                type={type}>
+                <HStack justifyContent="space-around">
+                  <TouchableOpacity
+                    style={{ paddingVertical: 20 }}
+                    onPress={() => {
+                      setType(type === CameraType.back ? CameraType.front : CameraType.back)
+                    }}>
+                    <MaterialIcons name="flip-camera-android" size={36} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ paddingVertical: 20 }}
+                    onPress={() => {
+                      takePicture()
+                    }}>
+                    <MaterialIcons name="photo-camera" size={36} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ paddingVertical: 20 }}
+                    onPress={() => {
+                      setIsCameraVisible(false)
+                    }}>
+                    <MaterialIcons name="cancel" size={36} color="white" />
+                  </TouchableOpacity>
+                </HStack>
+              </Camera>
+            )}
+            <Text style={styles.createAccountText}>
+              Preencha o máximo de dados a respeito do pet encontrado...
             </Text>
-            <Input
-              placeholder="Nome(opcional)"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Select
-              minWidth="200"
-              accessibilityLabel="sexo"
-              style={{ backgroundColor: 'white', opacity: 0.9, height: 51 }}
-              my={1}
-              placeholder="Selecione o sexo"
-              mt="1">
-              <Select.Item label="Macho" value="macho" />
-              <Select.Item label="Fêmea" value="femea" />
-            </Select>
-            <Input
-              placeholder="Idade(opcional)"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Input
-              placeholder="Raça(opcional)"
-              style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
-              my={2}
-            />
-            <Box alignItems="center" w="100%">
-              <TextArea
-                h={20}
-                placeholder="Descrição sobre o pet...*"
-                my={2}
-                fontSize={15}
-                autoCompleteType="off"
-              />
-            </Box>
-
-            <HStack justifyContent="space-between" alignItems="center">
-              <Text fontSize={14} color={theme.colors.primary[900]}>
-                Vacinado?
-              </Text>
-              <Switch
-                size={Platform.OS === 'ios' ? 'sm' : 'md'}
-                trackColor={{ true: theme.colors.primary[100] }}
-                thumbColor={isVacinated ? theme.colors.primary[600] : '#fff'}
-                onValueChange={() => toggleSwitch('vacinated')}
-                value={isVacinated}
-              />
-            </HStack>
-            <HStack justifyContent="space-between" alignItems="center">
-              <Text fontSize={14} color={theme.colors.primary[900]}>
-                Resgatado?
-              </Text>
-              <Switch
-                size={Platform.OS === 'ios' ? 'sm' : 'md'}
-                trackColor={{ true: theme.colors.primary[100] }}
-                thumbColor={isRescued ? theme.colors.primary[600] : '#fff'}
-                onValueChange={() => toggleSwitch('rescued')}
-                value={isRescued}
-              />
-            </HStack>
             <TouchableOpacity
-              // onPress={goToPetInformation}
-              style={{
-                marginTop: 50,
-                marginBottom: 40,
-                borderRadius: 20,
-                paddingHorizontal: 10,
-                paddingVertical: 10,
-                backgroundColor: theme.colors.primary[700]
+              onPress={() => {
+                sheetRef.current?.expand()
               }}>
-              <Text textAlign="center" color={theme.colors.white} bold>
-                Salvar
-              </Text>
+              <Image
+                source={image ? { uri: image } : AddDogPhoto}
+                alt="dog icon and a camera"
+                mb={3}
+                style={{ width: 125, height: 125, borderRadius: 20 }}
+              />
             </TouchableOpacity>
-          </View>
-        </VStack>
-      </Layouts.Internal>
-    </ScrollView>
+            <Text fontSize={15} color={theme.colors.primary[900]} my={2}>
+              Informações de endereço:
+            </Text>
+
+            <View>
+              <Input
+                placeholder="CEP*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Endereço*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Bairro*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Cidade*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Estado*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Número*"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Text fontSize={15} color={theme.colors.primary[900]} mt={5}>
+                Informações do pet:
+              </Text>
+              <Input
+                placeholder="Nome(opcional)"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Select
+                minWidth="200"
+                accessibilityLabel="sexo"
+                style={{ backgroundColor: 'white', opacity: 0.9, height: 51 }}
+                my={1}
+                placeholder="Selecione o sexo"
+                mt="1">
+                <Select.Item label="Macho" value="macho" />
+                <Select.Item label="Fêmea" value="femea" />
+              </Select>
+              <Input
+                placeholder="Idade(opcional)"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Input
+                placeholder="Raça(opcional)"
+                style={{ backgroundColor: 'white', opacity: 0.9, fontSize: 15, height: 51 }}
+                my={2}
+              />
+              <Box alignItems="center" w="100%">
+                <TextArea
+                  h={20}
+                  placeholder="Descrição sobre o pet...*"
+                  my={2}
+                  fontSize={15}
+                  autoCompleteType="off"
+                />
+              </Box>
+
+              <HStack justifyContent="space-between" alignItems="center">
+                <Text fontSize={14} color={theme.colors.primary[900]}>
+                  Vacinado?
+                </Text>
+                <Switch
+                  size={Platform.OS === 'ios' ? 'sm' : 'md'}
+                  trackColor={{ true: theme.colors.primary[100] }}
+                  thumbColor={isVacinated ? theme.colors.primary[600] : '#fff'}
+                  onValueChange={() => toggleSwitch('vacinated')}
+                  value={isVacinated}
+                />
+              </HStack>
+              <HStack justifyContent="space-between" alignItems="center">
+                <Text fontSize={14} color={theme.colors.primary[900]}>
+                  Resgatado?
+                </Text>
+                <Switch
+                  size={Platform.OS === 'ios' ? 'sm' : 'md'}
+                  trackColor={{ true: theme.colors.primary[100] }}
+                  thumbColor={isRescued ? theme.colors.primary[600] : '#fff'}
+                  onValueChange={() => toggleSwitch('rescued')}
+                  value={isRescued}
+                />
+              </HStack>
+              <TouchableOpacity
+                // onPress={goToPetInformation}
+                style={{
+                  marginTop: 50,
+                  marginBottom: 40,
+                  borderRadius: 20,
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  backgroundColor: theme.colors.primary[700]
+                }}>
+                <Text textAlign="center" color={theme.colors.white} bold>
+                  Salvar
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </VStack>
+        </Layouts.Internal>
+      </ScrollView>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        backgroundStyle={{ backgroundColor: theme.colors.primary[800] }}
+        handleIndicatorStyle={{ backgroundColor: '#CCC' }}
+        index={0}>
+        <BottomSheetView
+          style={{
+            backgroundColor: theme.colors.primary[800],
+            height: '100%',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 25,
+            paddingHorizontal: 20
+          }}>
+          <HStack justifyContent="space-around">
+            <TouchableOpacity
+              onPress={() => {
+                setIsCameraVisible(true)
+                sheetRef.current?.close()
+              }}>
+              <Image alt="camera" source={SheetCamera} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                pickImageFromGallery()
+                sheetRef.current?.close()
+              }}>
+              <Image alt="gallery" source={Gallery} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setImage(null)
+              }}>
+              <Image alt="remove photo" source={RemovePhoto} />
+            </TouchableOpacity>
+          </HStack>
+        </BottomSheetView>
+      </BottomSheet>
+    </>
   )
 }
 
